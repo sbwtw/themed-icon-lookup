fa2e7fadb018599186fc5bd77a0c4d372cbe961e
石博文 <sbw@sbw.so>
2018-05-03 19:37:17 +0800

    ffi: return nullptr if icon not found.

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,8 +2,25 @@
 use icon_lookup;
 
 use std::mem;
+use std::ptr;
 use std::os::raw::c_char;
 use std::ffi::{CStr, CString};
+
+macro_rules! c_strify {
+    ($pathbuf: expr) => {
+        match $pathbuf {
+            Some(path) => {
+                let icon = CString::new(path.to_string_lossy().as_ref()).unwrap();
+                let ptr = icon.as_ptr();
+
+                mem::forget(icon);
+
+                ptr
+            },
+            None => ptr::null()
+        }
+    };
+}
 
 #[no_mangle]
 pub extern "C" fn find_icon_with_theme_name(theme: *const c_char, icon: *const c_char, size: i32, scale: i32) -> *const c_char {
@@ -11,17 +28,7 @@ pub extern "C" fn find_icon_with_theme_name(theme: *const c_char, icon: *const c
     let theme = unsafe { CStr::from_ptr(theme).to_string_lossy() };
     let icon = unsafe { CStr::from_ptr(icon).to_string_lossy() };
 
-    let path = match icon_lookup::find_icon_with_theme_name(theme, icon, size, scale) {
-        Some(path) => {
-            CString::new(path.to_string_lossy().as_ref()).unwrap()
-        },
-        _ => CString::new("").unwrap(),
-    };
-
-    let ptr = path.as_ptr();
-    mem::forget(path);
-
-    ptr
+    c_strify!(icon_lookup::find_icon_with_theme_name(theme, icon, size, scale))
 }
 
 #[no_mangle]
@@ -29,17 +36,7 @@ pub extern "C" fn find_icon(icon: *const c_char, size: i32, scale: i32) -> *cons
 
     let icon = unsafe { CStr::from_ptr(icon).to_string_lossy() };
 
-    let path = match icon_lookup::find_icon(icon, size, scale) {
-        Some(path) => {
-            CString::new(path.to_string_lossy().as_ref()).unwrap()
-        },
-        _ => CString::new("").unwrap(),
-    };
-
-    let ptr = path.as_ptr();
-    mem::forget(path);
-
-    ptr
+    c_strify!(icon_lookup::find_icon(icon, size, scale))
 }
 
 #[no_mangle]
